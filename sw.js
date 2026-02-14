5947716ea310df06c5749d3235897c026ae8941b
mbsbooksandstationerymember-sudo <mbsbooksandstationerymember@gmail.com>
2026-02-14 19:30:09 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
+// ===== Service Worker (稳定版) =====
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/mbsmember/sw.js')
+    .then(() => navigator.serviceWorker.ready)
+    .then(() => {
+      console.log("Service Worker Ready & Controlling Page");
+    })
+    .catch(err => console.log("SW error:", err));
+}
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
